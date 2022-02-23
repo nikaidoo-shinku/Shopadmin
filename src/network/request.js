f0338ca7355e4a71e2c8a16ed41b534d0e2e2ed0
nikaidoo-shinku <1192490160@qq.com>
2022-02-23 18:03:26 +0800

    项目初步优化

--- src/network/request.js
+++ src/network/request.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+import nProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 export function request(config) {
     const instance = axios.create({
         baseURL: 'http://127.0.0.1:8888/api/private/v1/',
@@ -7,6 +10,9 @@ export function request(config) {
     })
 
     instance.interceptors.request.use(config => {
+        // 在request拦截器中，展示进度条nProgress.start()
+        nProgress.start()
+
         config.headers.Authorization = window.sessionStorage.getItem('token')
         return config
     }), err => {
@@ -14,6 +20,9 @@ export function request(config) {
     }
 
     instance.interceptors.response.use(res => {
+        // 在response拦截器中，隐藏进度条nProgress.done()
+        nProgress.done()
+
         return res.data
     }, err => {
         console.log(err);
